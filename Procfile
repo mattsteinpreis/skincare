b4e6b0bbc97c8ef6953aa6d421dee8c12eed5caf
Matt Steinpreis <matt.steinpreis@gmail.com>
2015-08-04 18:10:02 -0400

    Exclude ingredients, and sort by price or rating

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
-web: gunicorn hello:app --log-file=-
+
+web: gunicorn hello:app --log-file -
 
--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import pandas as pd
 
-def PickCategories(d, mycats):
+def PickCategories(d, mycats):  
     if not isinstance(mycats, list):
         mycats = [mycats]
     bools = []
@@ -12,8 +12,30 @@ def PickCategories(d, mycats):
             bools.append(True)
     return d[bools]
 
-def Tester(s):
-    print s
+def ExcludeIngredient(d, bad_ing):
+    if not isinstance(bad_ing,list):
+        bad_ing = [bad_ing]
+    bools = []
+    for i, inglist in enumerate(d.ingredients):
+        bools.append(True)
+        for ing in inglist:
+            for bad in bad_ing:
+                if bad.lower() in ing.lower():
+                    bools[i]=False
+                    break
+    return d[bools]
+
+def SortBy(d, choice):
+    if choice == 'pricelow':
+        return d.sort(['price'], ascending=[1])
+    elif choice == 'pricehigh':
+        return d.sort(['price'], ascending=[0])
+    elif choice == 'ratinglow':
+        return d.sort(['rating'], ascending=[1])
+    elif choice == 'ratinghigh':
+        return d.sort(['rating'], ascending=[0])
+    else:
+        return d
 
 app = Flask(__name__)
 
@@ -21,6 +43,7 @@ with open('AllProducts071715.txt', 'rb') as f:
     dsl = f.read().splitlines()
 data_json_str = "[" + ','.join(dsl) + "]"
 df = pd.read_json(data_json_str)
+df = df[['name','categories','ingredients','pros','cons','rating','nreviews','price']]
 df_show = df.copy()
 #cats = set([item for sublist in df.categories[:10] for item in sublist])
 
@@ -31,9 +54,23 @@ def hello():
 @app.route('/select', methods = ['POST'])
 def select():
     global df_show
+    
+    #pick a category
     cat = request.form['category']
     cat = str(cat)
-    df_show = PickCategories(df_show, cat)
+    if cat != "":
+        df_show = PickCategories(df_show, cat)
+    
+    #exclude an ingredient
+    ingr = request.form['ingredient']
+    ingr = str(ingr)
+    if ingr != "":
+        df_show = ExcludeIngredient(df_show,ingr)
+    
+    #sort by
+    sortby = request.form['sort']
+    df_show = SortBy(df_show,sortby)
+    
     return redirect('/')
 
 @app.route('/reset', methods = ['POST'])
